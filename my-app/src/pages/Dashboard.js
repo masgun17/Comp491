eba727d22e69924a1714e5b156b9c5161502c388
masgun17 <masgun17@ku.edu.tr>
2022-03-14 23:12:14 +0300

    Initial dashboard modifications

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -1,18 +1,17 @@
 const Dashboard = () => {
   return (
     // <h1>Dashboard</h1>;
-    <>
       <div className="dashboardLayout">
-        <h1>Dashboard</h1>
-        <div className="aboutTest">
+        <div className="dashboardDiv1" style={{"grid-row-start": "1"}}>
+            <h1>Anasayfa</h1>
+        </div>
+        <div className="dashboardDiv2" style={{"grid-row-start": "2"}}>
             Test Hakkında
         </div>
-        <div>
+        <div className="dashboardDiv3" style={{"grid-row-start": "3"}}>
             Neden böyle bir test yapma ihtiyacı duyduk?
         </div>
-        
       </div>
-    </>
   );
 };
 
