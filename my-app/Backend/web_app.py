5a22f8d971644076cfb0ba82b5bcf2f578ce5ff5
mefeyuz <mehmetefeyuzuguler@gmail.com>
2022-03-20 13:26:30 +0300

    backend + database installation
    Backend ve database bağlantıları oluşturuldu. Test için toplama yapan API yazıldı. Bundan sonra yapılması gereken DB'nin içini doldurmak

--- my-app/Backend/web_app.py
+++ my-app/Backend/web_app.py
@@ -1,15 +1,22 @@
+import urllib
+
 import flask
 from flask import Flask, request, url_for, jsonify
 import requests
 import react
 from react.render import render_component
 from werkzeug.utils import redirect
+from sqlalchemy import create_engine, text
+import pyodbc
 
 app = Flask("comp491")
 
 response = requests.get('https://httpbin.org/ip')
-print('Your IP is {0}'.format(response.json()['origin']))
+## print('Your IP is {0}'.format(response.json()['origin']))
 
+# Trusted Connection to Named Instance
+connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost\SQLEXPRESS;DATABASE=Comp491;Trusted_Connection=yes;')
+conn=connection.cursor()
 
 @app.route("/")
 def start():
@@ -31,10 +38,15 @@ def anasayfa():
 
 @app.route("/add",  methods=['GET'])
 def add():
-    num1 = float(request.form["num1"])
-    num2 = float(request.form["num2"])
-
-    sum = num1 + num2
+    num1 = int(request.form["num1"])
+    num2 = int(request.form["num2"])
+    print(num1)
+    print(num2)
+    number1 = conn.execute(f"SELECT value FROM Numbers where Id = {num1};").fetchall()
+    number2 = conn.execute(f"SELECT value FROM Numbers where Id = {num2};").fetchall()
+    print(number1[0][0])
+    print(number2[0][0])
+    sum = number1[0][0] + number2[0][0]
     print(sum)
     return {"Summation": sum}
 
